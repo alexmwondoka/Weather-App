f01014b50c44e8e365d34a9c1052569ffcfadf0d
alexandraa101 <150080324+alexmwondoka@users.noreply.github.com>
2024-07-19 11:26:16 +0200

    Added basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Hey");
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityName = document.querySelector(".city");
+  cityName.innerHTML = searchInput.value;
+  console.log(cityName);
+}
+
+let searchElement = document.querySelector("#weather-form");
+searchElement.addEventListener("submit", handleSearch);
