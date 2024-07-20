a1fe8eefbb2b6b7be5c7edf973097f77ec6c5c9c
alexandraa101 <150080324+alexmwondoka@users.noreply.github.com>
2024-07-20 22:33:49 +0200

    Updated search engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city-name");
+
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a25a30d9ff54c846ecdc270dcb8coa0t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityName = document.querySelector(".city");
-  cityName.innerHTML = searchInput.value;
-  console.log(cityName);
+
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#weather-form");
 searchElement.addEventListener("submit", handleSearch);
-
